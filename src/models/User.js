5f28ca477f36ea072b9694b908cef8ffc18be9ba
taijylangu30-lgtm <taijylangu30@gmail.com>
2026-08-28 13:41:55 +0100

    Create User model with mongoose schema

---
+++ src/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  anonymousId: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now },
+  lastActive: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
